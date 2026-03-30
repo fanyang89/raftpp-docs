3ae1b9ce7a82acb3333aa6703bf428cdc00c746c
fanyang89 <fanyang89@outlook.com>
2026-03-30 22:02:52 +0800

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://fanyang89.github.io',
+	base: '/raftpp-docs',
 	integrations: [
 		starlight({
 			title: 'My Docs',
